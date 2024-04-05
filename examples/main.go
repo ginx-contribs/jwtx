2c410f48052cb224b64323f195230339851eb1ac
yihhao wang <2633565580@qq.com>
2024-04-05 14:05:08 +0800

    docs(examples): update examples

---
+++ examples/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/ginx-contribs/jwtx"
+	"github.com/golang-jwt/jwt/v5"
+	"log"
+)
+
+func main() {
+	key := []byte("my key")
+	token, err := jwtx.Issue(key, jwt.SigningMethodHS256)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(token.SignedString)
+
+	verifiedToken, err := jwtx.Verify(token.SignedString, key, jwt.SigningMethodHS256)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(verifiedToken.SignedString)
+}
